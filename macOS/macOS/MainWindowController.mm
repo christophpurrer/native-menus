aa91e936074449cbcb635845dad7ccf1aa66a433
Christoph Purrer <christophpurrer@gmail.com>
2021-03-10 16:38:29 -0800

    Update clang-format

--- macOS/macOS/MainWindowController.mm
+++ macOS/macOS/MainWindowController.mm
@@ -12,77 +12,80 @@
 }
 
 - (instancetype)init {
-  // Nib name must be non-nil: http://www.openradar.me/19289232
-  if (self = [super initWithWindowNibName:@""]) {
-    self.windowFrameAutosaveName = @"MainWindow";
-  }
-  return self;
+    // Nib name must be non-nil: http://www.openradar.me/19289232
+    if (self = [super initWithWindowNibName:@""]) {
+        self.windowFrameAutosaveName = @"MainWindow";
+    }
+    return self;
 }
 
 - (IBAction)menuItemClicked:(id)sender {
-  if ([sender
-          conformsToProtocol:@protocol(NSUserInterfaceItemIdentification)] &&
-      [sender identifier]) {
-    NSLog(@"menuItem: %@", ((NSMenuItem *)sender).title);
-  }
+    if ([sender
+            conformsToProtocol:@protocol(NSUserInterfaceItemIdentification)] &&
+        [sender identifier]) {
+        NSLog(@"menuItem: %@", ((NSMenuItem *)sender).title);
+    }
 }
 
 void createAppMenu() {
-  // https://medium.com/@theboi/macos-apps-without-storyboard-or-xib-menu-bar-in-swift-5-menubar-and-toolbar-6f6f2fa39ccb
-  auto applicationMenus = createApplicationMenu();
-  NSMenu *menubar = [NSMenu new];
-  for (auto const &applicationMenu : applicationMenus) {
-    NSMenuItem *menuItem = [NSMenuItem new];
-    [menubar addItem:menuItem];
-    NSMenu *menu = [NSMenu new];
-    menu.title = [NSString stringWithUTF8String:applicationMenu.title.c_str()];
+    // https://medium.com/@theboi/macos-apps-without-storyboard-or-xib-menu-bar-in-swift-5-menubar-and-toolbar-6f6f2fa39ccb
+    auto applicationMenus = createApplicationMenu();
+    NSMenu *menubar = [NSMenu new];
+    for (auto const &applicationMenu : applicationMenus) {
+        NSMenuItem *menuItem = [NSMenuItem new];
+        [menubar addItem:menuItem];
+        NSMenu *menu = [NSMenu new];
+        menu.title =
+            [NSString stringWithUTF8String:applicationMenu.title.c_str()];
 
-    for (auto &menuEntry : applicationMenu.entries) {
-      auto menuEntryItem = dynamic_cast<MenuEntry *>(menuEntry.get());
-      if (menuEntryItem) { //
-        NSString *title =
-            [NSString stringWithUTF8String:menuEntryItem->title.c_str()];
-        NSMenuItem *subMenuItem =
-            [[NSMenuItem alloc] initWithTitle:title
-                                       action:@selector(menuItemClicked:)
-                                keyEquivalent:@""];
-        [subMenuItem setEnabled:menuEntryItem->enabled ? YES : NO];
-        [menu addItem:subMenuItem];
-      }
-      auto menuEntrySeparator = dynamic_cast<Separator *>(menuEntry.get());
-      if (menuEntrySeparator) {
-        [menu addItem:[NSMenuItem separatorItem]];
-      }
+        for (auto &menuEntry : applicationMenu.entries) {
+            auto menuEntryItem = dynamic_cast<MenuEntry *>(menuEntry.get());
+            if (menuEntryItem) { //
+                NSString *title = [NSString
+                    stringWithUTF8String:menuEntryItem->title.c_str()];
+                NSMenuItem *subMenuItem = [[NSMenuItem alloc]
+                    initWithTitle:title
+                           action:@selector(menuItemClicked:)
+                    keyEquivalent:@""];
+                [subMenuItem setEnabled:menuEntryItem->enabled ? YES : NO];
+                [menu addItem:subMenuItem];
+            }
+            auto menuEntrySeparator =
+                dynamic_cast<Separator *>(menuEntry.get());
+            if (menuEntrySeparator) {
+                [menu addItem:[NSMenuItem separatorItem]];
+            }
+        }
+        [menuItem setSubmenu:menu];
     }
-    [menuItem setSubmenu:menu];
-  }
-  NSApplication.sharedApplication.menu = menubar;
+    NSApplication.sharedApplication.menu = menubar;
 }
 
 - (void)loadWindow {
-  self.window = [[NSWindow alloc]
-      initWithContentRect:NSMakeRect(0,
-                                     [[NSScreen mainScreen] frame].size.height -
-                                         640,
-                                     360, 640)
-                styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
-                          NSWindowStyleMaskResizable
-                  backing:NSBackingStoreBuffered
-                    defer:NO];
-  self.window.title = @"App";
-  self.window.delegate = self;
-  [self.window makeKeyAndOrderFront:NULL];
-  // Default the window to the center of the screen.
-  [NSMenu setMenuBarVisible:YES];
-  [self.window center];
-  createAppMenu();
+    self.window = [[NSWindow alloc]
+        initWithContentRect:NSMakeRect(
+                                0,
+                                [[NSScreen mainScreen] frame].size.height - 640,
+                                360, 640)
+                  styleMask:NSWindowStyleMaskTitled |
+                            NSWindowStyleMaskClosable |
+                            NSWindowStyleMaskResizable
+                    backing:NSBackingStoreBuffered
+                      defer:NO];
+    self.window.title = @"App";
+    self.window.delegate = self;
+    [self.window makeKeyAndOrderFront:NULL];
+    // Default the window to the center of the screen.
+    [NSMenu setMenuBarVisible:YES];
+    [self.window center];
+    createAppMenu();
 }
 
 #pragma mark - NSWindowDelegate
 
 - (BOOL)windowShouldClose:(NSWindow *)window {
-  [window orderOut:self];
-  return NO;
+    [window orderOut:self];
+    return NO;
 }
 
 @end
--- shared/Menu.h
+++ shared/Menu.h
@@ -11,58 +11,56 @@
 #include <vector>
 
 struct IMenuEntry {
-  virtual ~IMenuEntry(){};
+    virtual ~IMenuEntry(){};
 };
 
 struct MenuEntry : IMenuEntry {
-  MenuEntry(const std::string &_title, bool _enabled)
-      : IMenuEntry(), title(_title), enabled(_enabled) {}
-  MenuEntry(const std::string &_title) : MenuEntry(_title, true) {}
-  std::string title;
-  bool enabled;
+    MenuEntry(const std::string& _title, bool _enabled)
+        : IMenuEntry(), title(_title), enabled(_enabled) {}
+    MenuEntry(const std::string& _title) : MenuEntry(_title, true) {}
+    std::string title;
+    bool enabled;
 };
 
 struct Separator : IMenuEntry {
-  Separator() {}
+    Separator() {}
 };
 
 struct Menu {
-  std::string title;
-  std::vector<std::unique_ptr<IMenuEntry>> entries;
+    std::string title;
+    std::vector<std::unique_ptr<IMenuEntry>> entries;
 };
 
 static inline std::vector<Menu> createApplicationMenu() {
-  std::vector<Menu> appMenu;
-  // App menu
-  {
-    auto menu = Menu{"Application"};
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Preference"));
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Hide Others"));
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Check for Updates"));
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Quit Application"));
-    appMenu.emplace_back(std::move(menu));
-  }
+    std::vector<Menu> appMenu;
+    // App menu
+    {
+        auto menu = Menu{"Application"};
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Preference"));
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Hide Others"));
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Check for Updates"));
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Quit Application"));
+        appMenu.emplace_back(std::move(menu));
+    }
 
-  // File
-  {
-    auto menu = Menu{"File"};
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("New Document"));
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Log Out"));
-    menu.entries.emplace_back(std::make_unique<Separator>());
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("Close Window"));
-    appMenu.emplace_back(std::move(menu));
-  }
+    // File
+    {
+        auto menu = Menu{"File"};
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("New Document"));
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Log Out"));
+        menu.entries.emplace_back(std::make_unique<Separator>());
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Close Window"));
+        appMenu.emplace_back(std::move(menu));
+    }
 
-  // Help
-  {
-    auto menu = Menu{"Help"};
-    menu.entries.emplace_back(std::make_unique<MenuEntry>("About Application"));
-    menu.entries.emplace_back(
-        std::make_unique<MenuEntry>("Factory reset", false));
-    menu.entries.emplace_back(std::make_unique<Separator>());
-    menu.entries.emplace_back(
-        std::make_unique<MenuEntry>("Submit Feedback", false));
-    appMenu.emplace_back(std::move(menu));
-  }
-  return appMenu;
+    // Help
+    {
+        auto menu = Menu{"Help"};
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("About Application"));
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Factory reset", false));
+        menu.entries.emplace_back(std::make_unique<Separator>());
+        menu.entries.emplace_back(std::make_unique<MenuEntry>("Submit Feedback", false));
+        appMenu.emplace_back(std::move(menu));
+    }
+    return appMenu;
 }
