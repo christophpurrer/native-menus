d66ea2c890b97af2e154d0d79d3e2200196066aa
Christoph Purrer <christophpurrer@gmail.com>
2021-03-10 16:29:38 -0800

    Clang format

--- macOS/macOS/AppDelegate.mm
+++ macOS/macOS/AppDelegate.mm
@@ -9,21 +9,21 @@
 #import "MainWindowController.h"
 
 @implementation AppDelegate {
-  MainWindowController* _windowController;
+  MainWindowController *_windowController;
 }
 
-- (void)applicationWillFinishLaunching:(NSNotification*)__unused aNotification {
-    _windowController = [MainWindowController new];
+- (void)applicationWillFinishLaunching:(NSNotification *)__unused
+    aNotification {
+  _windowController = [MainWindowController new];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    [_windowController showWindow:self];
-    // Insert code here to initialize your application
+  [_windowController showWindow:self];
+  // Insert code here to initialize your application
 }
 
-
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+  // Insert code here to tear down your application
 }
 
 @end
