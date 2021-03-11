7de8cbdccb1297fc4ff4dd8153aaa3e93178389d
Christoph Purrer <christophpurrer@gmail.com>
2021-03-10 17:31:33 -0800

    Update win32 sample

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Programmatically generating menu entries for macOS and Windows
 
 ## macOS demo
 ![macOS Demo](macOS.png)
+
+## win32 demo
+![win32 Demo](win32.png)
--- win32/win32/main.cpp
+++ win32/win32/main.cpp
@@ -1,22 +1,32 @@
+#include "../../shared/Menu.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 #include <windows.h>
 
+// https://stackoverflow.com/a/47312126
+class dbg_stream_for_cout : public std::stringbuf {
+  public:
+    ~dbg_stream_for_cout() { sync(); }
+    int sync() {
+        ::OutputDebugStringA(str().c_str());
+        str(std::string()); // Clear the string buffer
+        return 0;
+    }
+};
+dbg_stream_for_cout g_DebugStreamFor_cout;
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void AddMenus(HWND);
-void AddMenus2(HWND);
+void createAppMenu(HWND);
 
-#define IDM_FILE_NEW 1
-#define IDM_FILE_OPEN 2
-#define IDM_FILE_QUIT 3
-#define IDM_IMPORT_MAIL 11
-
-#define APPWM_ICONNOTIFY (WM_APP + 1)
+static std::unordered_map<int32_t, std::string> sMenuIdToMenuKey;
 
 // https://zetcode.com/gui/winapi/menus/
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
-
     MSG msg;
     WNDCLASSW wc = {0};
-    wc.lpszClassName = L"Messengefffr";
+    wc.lpszClassName = L"Win32App";
     wc.hInstance = hInstance;
     wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
     wc.lpfnWndProc = WndProc;
@@ -24,28 +34,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
     wc.hIcon = LoadIcon(wc.hInstance, MAKEINTRESOURCE(1));
 
     RegisterClassW(&wc);
-    auto hwnd = CreateWindowW(wc.lpszClassName, L"Simple menu", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                              100, 100, 350, 250, 0, 0, hInstance, 0);
-
-    SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)wc.hIcon);
-    SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)wc.hIcon);
-
-    // Notification
-    NOTIFYICONDATA nid = {};
-    nid.cbSize = sizeof(nid);
-    nid.hWnd = hwnd;
-    nid.uID = 1;
-    nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
-    nid.uCallbackMessage = APPWM_ICONNOTIFY;
-    nid.hIcon = wc.hIcon;
-    // This text will be shown as the icon's tooltip.
-    // StringCchCopy(nid.szTip, ARRAYSIZE(nid.szTip), "d");
-
-    // Show the notification.
-    Shell_NotifyIcon(NIM_ADD, &nid);
+    auto hwnd = CreateWindowW(wc.lpszClassName, L"Win32", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100,
+                              100, 350, 250, 0, 0, hInstance, 0);
 
     while (GetMessage(&msg, NULL, 0, 0)) {
-
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -54,42 +46,23 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-
     switch (msg) {
 
-    case APPWM_ICONNOTIFY:
-
-        break;
-
     case WM_CREATE: {
-        HICON hIcon = static_cast<HICON>(
-            LoadImage(NULL, TEXT("Icon\\small.ico"), IMAGE_ICON, 0, 0,
-                      LR_DEFAULTCOLOR | LR_SHARED | LR_DEFAULTSIZE | LR_LOADFROMFILE));
-        SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-        AddMenus(hwnd);
+        createAppMenu(hwnd);
+        std::cout.rdbuf(&g_DebugStreamFor_cout);
         break;
     }
 
-    case WM_COMMAND:
-
-        switch (LOWORD(wParam)) {
-
-        case IDM_FILE_NEW:
-        case IDM_FILE_OPEN:
-
-            AddMenus2(hwnd);
-            break;
-
-        case IDM_FILE_QUIT:
-
-            SendMessage(hwnd, WM_CLOSE, 0, 0);
-            break;
+    case WM_COMMAND: {
+        auto it = sMenuIdToMenuKey.find(wParam);
+        if (it != sMenuIdToMenuKey.end()) {
+            std::cout << "menuItem: " << it->second << std::endl;
         }
-
         break;
+    }
 
     case WM_DESTROY:
-
         PostQuitMessage(0);
         break;
 
@@ -100,42 +73,32 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-void AddMenus(HWND hwnd) {
-
-    HMENU hMenubar;
-    HMENU hMenu;
-    HMENU hSubMenu = CreatePopupMenu();
-
-    hMenubar = CreateMenu();
-    hMenu = CreateMenu();
-
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_NEW, L"&New");
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Open");
-    AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Quit");
-
-    AppendMenuW(hMenu, MF_STRING | MF_POPUP, (UINT_PTR)hSubMenu, L"&Import");
-    AppendMenuW(hSubMenu, MF_STRING, IDM_IMPORT_MAIL, L"Import &mail");
-
-    AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&File");
-    SetMenu(hwnd, hMenubar);
-}
-
-void AddMenus2(HWND hwnd) {
-
-    HMENU hMenubar;
-    HMENU hMenu;
-
-    hMenubar = CreateMenu();
-    hMenu = CreateMenu();
-
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_NEW, L"&New");
-    AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Open");
-    AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
-    AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Quieeet");
-    AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
-
-    AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&Fileeee");
+void createAppMenu(HWND hwnd) {
+    auto applicationMenus = createApplicationMenu();
+    HMENU hMenubar = CreateMenu();
+    int32_t menuId = 1023;
+    for (auto const& applicationMenu : applicationMenus) {
+        HMENU hMenu = CreateMenu();
+        for (auto& menuEntry : applicationMenu.entries) {
+            auto menuEntryItem = dynamic_cast<MenuEntry*>(menuEntry.get());
+            if (menuEntryItem) { //
+                auto flags = MF_STRING;
+                if (!menuEntryItem->enabled) {
+                    flags |= MF_DISABLED;
+                }
+                AppendMenuW(
+                    hMenu, flags, menuId,
+                    std::wstring(menuEntryItem->title.begin(), menuEntryItem->title.end()).c_str());
+                sMenuIdToMenuKey[menuId++] = menuEntryItem->title;
+            }
+            auto menuEntrySeparator = dynamic_cast<Separator*>(menuEntry.get());
+            if (menuEntrySeparator) {
+                AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
+            }
+        }
+        AppendMenuW(
+            hMenubar, MF_POPUP, (UINT_PTR)hMenu,
+            std::wstring(applicationMenu.title.begin(), applicationMenu.title.end()).c_str());
+    }
     SetMenu(hwnd, hMenubar);
 }
